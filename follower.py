b6e0114264d76ba0b23a333d763b6d0b962843aa
will <williangalvani@gmail.com>
2015-10-20 14:35:44 -0200

    .

--- follower.py
+++ follower.py
@@ -2,9 +2,11 @@ __author__ = 'will'
 
 from interface import RobotInterface
 import time
-import numpy
+import numpy as np
 import cv2
 from scipy import ndimage
+from thining import thinning
+
 RESX = 128
 RESY = 128
 
@@ -47,25 +49,28 @@ class Controller:
         img = None
 
         while not img:  img = self.interface.read_camera()
-        img = numpy.array(img[2], dtype='int8').reshape(128, 128)
+        img = np.array(img[2], dtype='uint8').reshape(128, 128)
         cv2.imshow("camera", img[40:])
 
-        ret,thresh2 = cv2.threshold(img.astype(numpy.uint8),127,255,cv2.THRESH_BINARY_INV)
+        h, w = img.shape[:2]
+        threshold = int(np.mean(img[h/1.5]))*0.5
+        print threshold
 
-        #mask = numpy.zeros((128, 128), numpy.uint8)
 
-        colored = cv2.cvtColor(thresh2,cv2.COLOR_GRAY2RGB)
+        ret, thresh2 = cv2.threshold(img.astype(np.uint8), threshold, 255, cv2.THRESH_BINARY_INV)
+        erosion = thinning(thresh2)
+
+        colored = cv2.cvtColor(erosion.astype(np.uint8), cv2.COLOR_GRAY2RGB)
 
         tracker = []
 
-
-
         for y, line in enumerate(thresh2):
             cx = ndimage.measurements.center_of_mass(line)
-            if 4<numpy.mean(line)<50:
+            if 4<np.mean(line)<50:
                 tracker.append((int(cx[0]),y))
-                if not numpy.isnan(cx[0]):
-                    cv2.circle(colored, (int(cx[0]), y ), 1, (255, 0, 0), 2)
+                if not np.isnan(cx[0]):
+                    pass
+                    cv2.circle(colored, (int(cx[0]), y), 1, (255, 0, 0), 2)
         #cv2.imshow("target", colored)
         return tracker, colored
 
---
+++ path.py
@@ -0,0 +1,19 @@
+__author__ = 'will'
+
+import numpy as np
+
+class Node():
+    child_nodes = []
+
+
+class Path():
+    root = None
+
+    def __init__(self, bw_image):
+
+
+         for line in reversed(bw_image):
+             for pixel in line:
+
+
+
